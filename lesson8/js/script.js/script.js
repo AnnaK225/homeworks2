96459d10f9648661e9e458a40b7de2e7a8734634
Anna Kusnetsova <annakusnetsov@gmal.com>
2021-11-28 18:42:02 +0300

    add lesson8

---
+++ lesson8/js/script.js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider1').slick({ 
+        arrows:true,
+        dots:true,
+        adaptiveHeigh:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:500,
+        easing:'ease',
+        infinite:true,
+        autoplaySpeed:1500,
+    });    
+}); 
